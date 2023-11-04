d15f731bcfdadd5d585b58558eb134fef8009513
Mark Yu <zyu347@uwo.ca>
2023-11-04 03:43:23 -0400

    finished landing page

--- resubot-app/src/pages/Landing/Landing.jsx
+++ resubot-app/src/pages/Landing/Landing.jsx
@@ -8,6 +8,7 @@ const getItems = () =>
         .fill(0)
         .map((_, ind) => ({id: `feature-${ind}`}));
 
+
 // shown when
 const LandingPage = () => {
 
@@ -39,18 +40,18 @@ const LandingPage = () => {
                     </p>
 
                     <section className="section scroller-container is-align-items-center	">
-                    {/* Horizontal Scroller*/}
-                    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-                        {scollItems.map(({id}) => (
-                            <ScrollCard
-                                itemId={id} // NOTE: itemId is required for track items
-                                title={id}
-                                key={id}
-                                onClick={handleScrollItemClick(id)}
-                                selected={isItemSelected(id)}
-                            />
-                        ))}
-                    </ScrollMenu>
+                        {/* Horizontal Scroller*/}
+                        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+                            {scollItems.map(({id}) => (
+                                <ScrollCard
+                                    itemId={id} // NOTE: itemId is required for track items
+                                    title={id}
+                                    key={id}
+                                    onClick={handleScrollItemClick(id)}
+                                    selected={isItemSelected(id)}
+                                />
+                            ))}
+                        </ScrollMenu>
                     </section>
                     {/* Get Started Button*/}
                     <div className="columns is-centered">
@@ -59,8 +60,204 @@ const LandingPage = () => {
                 </div>
             </section>
 
-            <div className="example-container box">
+            <div className="examples-container box">
+                <h1 className="title">Example Resumes</h1>
+                <div className="examples-grid-container">
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
 
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
+
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
+
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
+
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
+
+                    <div className="example-box">
+                        <article className="message">
+                            <div className="message-header">
+                                <p>Hello World</p>
+                            </div>
+                            <div className="message-body">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus
+                                mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                                fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean
+                                ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin
+                                porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna,
+                                vehicula et sem eget, facilisis sodales sem.
+                            </div>
+                        </article>
+                    </div>
+                </div>
+
+                {/* Live data based on users*/}
+                <div className="user-data-wrapper">
+                    <h1 className="title">Live User Data</h1>
+                    {/*Number of resumes created over time.*/}
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Number of resumes created over time
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
+                                    <span className="icon">
+                                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                            </button>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                insert apex chart component
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Average time spent on creating a resume*/}
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Average time spent on creating a resume
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
+                                    <span className="icon">
+                                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                            </button>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                insert apex chart component
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Most frequently used keywords in resumesAverage time spent on creating a resume*/}
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Most frequently used keywords in resumes
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
+                                    <span className="icon">
+                                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                            </button>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                insert apex chart component
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Work experience distribution */}
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Work experience distribution (years of experience).
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
+                                    <span className="icon">
+                                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                            </button>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                insert apex chart component
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Education level distribution*/}
+                    <div className="card">
+                        <header className="card-header">
+                            <p className="card-header-title">
+                                Education level distribution
+                            </p>
+                            <button className="card-header-icon" aria-label="more options">
+                                    <span className="icon">
+                                      <i className="fas fa-angle-down" aria-hidden="true"></i>
+                                    </span>
+                            </button>
+                        </header>
+                        <div className="card-content">
+                            <div className="content">
+                                insert apex chart component
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
