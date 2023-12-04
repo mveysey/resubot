bd5df3476d251e0eafe9ba8ffa12395c33371f0f
mveysey <mveysey@uwo.ca>
2023-12-04 08:21:27 -0500

    Change Server port to 4000

--- Server/routes/test.router.js
+++ Server/routes/test.router.js
@@ -1,9 +1,9 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-//http://localhost:5000/api/test/hello
-const testController = require("../controller/test.controller")
+//http://localhost:4000/api/test/hello
+const testController = require("../controller/test.controller");
 
-router.get("/hello", testController.test1)
+router.get("/hello", testController.test1);
 
-module.exports = router
+module.exports = router;
--- resubot-app/src/common/axiosConfig.js
+++ resubot-app/src/common/axiosConfig.js
@@ -1,31 +1,31 @@
-import _axios from 'axios';
+import _axios from "axios";
 
 //Axios, a promise based HTTP client for the browser and Node. js
-import auth from './auth.js'
+import auth from "./auth.js";
 
-const axios = baseURL => {
-    const instance = _axios.create({
-        baseURL:
-            baseURL || process.env.REACT_APP_API_DOMAIN || 'http://localhost:5000',
-        timeout: 3000
-    });
+const axios = (baseURL) => {
+  const instance = _axios.create({
+    baseURL:
+      baseURL || process.env.REACT_APP_API_DOMAIN || "http://localhost:4000",
+    timeout: 3000,
+  });
 
-    instance.interceptors.request.use(
-        config => {
-            const jwToken = auth.getToken();
-            config.headers['Authorization'] = 'Bearer ' + jwToken;
-            //Do something before request is sent
-            return config;
-        },
-        error => {
-            //Do something with request error
-            return Promise.reject(error);
-        }
-    );
+  instance.interceptors.request.use(
+    (config) => {
+      const jwToken = auth.getToken();
+      config.headers["Authorization"] = "Bearer " + jwToken;
+      //Do something before request is sent
+      return config;
+    },
+    (error) => {
+      //Do something with request error
+      return Promise.reject(error);
+    }
+  );
 
-    return instance;
+  return instance;
 };
 
-export {axios};
+export { axios };
 
 export default axios();
