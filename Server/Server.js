d59e870959be07e0185c01938d6c6b24cbd399f8
mveysey <mveysey@uwo.ca>
2023-12-03 21:04:39 -0500

    update api call to work with gpt-3.5-turbo

--- Server/Server.js
+++ Server/Server.js
@@ -1,14 +1,10 @@
+const { OpenAI } = require("openai");
 const express = require("express");
 const app = express();
 const routes = require("./Database"); // Import your routes
 const cors = require("cors");
-// const { Configuration, OpenAIApi } = require("openai");
 
-// const configuration = new Configuration({
-
-// });
-
-// const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,97 +20,98 @@ app.get("/api", (req, res) => {
   });
 });
 
-// const generateText = async (text) => {
-//   const response = await openai.createCompletion({
-//     model: "gpt-3.5-turbo",
-//     prompt: text,
-//     temperature: 0.6,
-//     max_tokens: 250,
-//     top_p: 1,
-//     frequency_penalty: 1,
-//     presence_penalty: 1,
-//   });
-//   return response.data.choices[0].text;
-// };
+const generateID = () => {
+  return Math.random(1000);
+};
 
-// // get submitted info
-// app.post("/resume/create", async (req, res) => {
-//   const {
-//     newPosition,
-//     industry,
-//     fullName,
-//     phoneNumber,
-//     email,
-//     linkedIn,
-//     personalLink,
-//     role1,
-//     company1,
-//     date1,
-//     location1,
-//     description1,
-//     role2,
-//     company2,
-//     date2,
-//     location2,
-//     description2,
-//     degree,
-//     location,
-//     schoolName,
-//     graduation,
-//     skills,
-//     projectTitle,
-//     projectDescription,
-//   } = req.body;
+// get generated text from openAI API based on given prompt
+const generateText = async (text) => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: text }],
+    temperature: 0.6,
+    max_tokens: 250,
+    top_p: 1,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+  return response.choices[0].message;
+};
 
-//   const newEntry = {
-//     id: generateID(),
-//     newPosition,
-//     industry,
-//     fullName,
-//     phoneNumber,
-//     email,
-//     linkedIn,
-//     personalLink,
-//     role1,
-//     company1,
-//     date1,
-//     location1,
-//     description1,
-//     role2,
-//     company2,
-//     date2,
-//     location2,
-//     description2,
-//     degree,
-//     location,
-//     schoolName,
-//     graduation,
-//     skills,
-//     projectTitle,
-//     projectDescription,
-//   };
+// get submitted info
+app.post("/resume/create", async (req, res) => {
+  (fullName = req.body.fullName),
+    (newPosition = req.body.newPosition),
+    (industry = req.body.industry),
+    (role1 = req.body.role1),
+    (company1 = req.body.company1),
+    (date1 = req.body.date1),
+    (location1 = req.body.location1),
+    (description1 = req.body.description1),
+    (role2 = req.body.role2),
+    (company2 = req.body.company2),
+    (date2 = req.body.date2),
+    (location2 = req.body.location2),
+    (description2 = req.body.descriotion2),
+    (degree = req.body.degree),
+    (location = req.body.location),
+    (schoolName = req.body.schoolName),
+    (graduation = req.body.graduation),
+    (skills = req.body.skilss),
+    (projectTitle = req.body.projectTitle),
+    (projectDescription = req.body.projectDescription);
 
-//   const prompt1 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write a 100 words description for the top of the resume(first person writing)?`;
+  const newEntry = {
+    id: generateID(),
+    fullName,
+    newPosition,
+    industry,
+    role1,
+    company1,
+    date1,
+    location1,
+    description1,
+    role2,
+    company2,
+    date2,
+    location2,
+    description2,
+    degree,
+    location,
+    schoolName,
+    graduation,
+    skills,
+    projectTitle,
+    projectDescription,
+  };
 
-//   const prompt2 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write 10 points for a resume on what I am good at?`;
+  const testPrompt = `My full name is ${fullName}. Can you write a 20 word description about my name?`;
 
-//   const prompt3 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n During my years I worked at ${
-//     workArray.length
-//   } companies. ${remainderText()} \n Can you write me 50 words for each company seperated in numbers of my succession in the company (in first person)?`;
+  // const educationInfo = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write a 100 words description for the top of the resume(first person writing)?`;
 
-//   const objective = await generateText(prompt1);
-//   const keypoints = await generateText(prompt2);
-//   const jobResponsibilities = await generateText(prompt3);
+  // const prompt2 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n I write in the technolegies: ${currentTechnologies}. Can you write 10 points for a resume on what I am good at?`;
 
-//   const chatgptData = { objective, keypoints, jobResponsibilities };
-//   const data = { ...newEntry, ...chatgptData };
-//   database.push(data);
+  // const prompt3 = `I am writing a resume, my details are \n name: ${fullName} \n role: ${currentPosition} (${currentLength} years). \n During my years I worked at ${
+  //   workArray.length
+  // } companies. ${remainderText()} \n Can you write me 50 words for each company seperated in numbers of my succession in the company (in first person)?`;
 
-//   res.json({
-//     message: "Request successful!",
-//     data,
-//   });
-// });
+  // const objective = await generateText(prompt1);
+  // const keypoints = await generateText(prompt2);
+  // const jobResponsibilities = await generateText(prompt3);
+
+  const test = await generateText(testPrompt);
+
+  // const chatgptData = { objective, keypoints, jobResponsibilities };
+  const chatgptData = { test };
+  const data = { ...newEntry, ...chatgptData };
+  // database.push(data);
+
+  console.log(data);
+  res.json({
+    message: "Request successful!",
+    data,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port 4000`);
