f4d19f0edd647830372dad82755db2485158721a
mveysey <mveysey@uwo.ca>
2023-12-04 17:13:18 -0500

    Write gpt prompts to include new information

--- Server/controller/resume.controller.js
+++ Server/controller/resume.controller.js
@@ -1,57 +1,46 @@
+const bodyParser = require("body-parser");
 const { generateID, generateText } = require("../util/ai-util"); // Import utility functions
 
 const resumeController = {
   // Create a resume entry logic
   createResumeEntry: async (req, res) => {
     try {
-      const {
-        fullName,
-        phoneNumber,
-        email,
-        linkedIn,
-        personalLink,
-        optionalJobDecsription,
-        optionalIndustry,
-        role1,
-        company1,
-        date1,
-        location1,
-        description1,
-        role2,
-        company2,
-        date2,
-        location2,
-        description2,
-        degree,
-        location,
-        schoolName,
-        graduation,
-        skills,
-        projectTitle,
-        projectDescription,
-      } = req.body;
+      (optionalJobDecsription = req.body.newPosition),
+        (optionalIndustry = req.body.industry),
+        (fullName = req.body.fullName),
+        (phoneNumber = req.body.phoneNumber),
+        (email = req.body.email),
+        (linkedIn = req.body.linkedIn),
+        (personalLink = req.body.personalLink),
+        (role = req.body.role),
+        (company = req.body.company),
+        (date = req.body.date),
+        (location = req.body.location),
+        (description = req.body.description),
+        (degree = req.body.degree),
+        (schoolLocation = req.body.schoolLocation),
+        (schoolName = req.body.schoolName),
+        (graduation = req.body.graduation),
+        (skills = req.body.skills),
+        (projectTitle = req.body.projectTitle),
+        (projectDescription = req.body.projectDescription);
 
       const newEntry = {
         id: generateID(),
+        optionalJobDecsription,
+        optionalIndustry,
         fullName,
         phoneNumber,
         email,
         linkedIn,
         personalLink,
-        optionalJobDecsription,
-        optionalIndustry,
-        role1,
-        company1,
-        date1,
-        location1,
-        description1,
-        role2,
-        company2,
-        date2,
-        location2,
-        description2,
-        degree,
+        role,
+        company,
+        date,
         location,
+        description,
+        degree,
+        schoolLocation,
         schoolName,
         graduation,
         skills,
@@ -62,7 +51,7 @@ const resumeController = {
       // assign values to optional params
       var jobDescription;
       switch (optionalJobDecsription) {
-        case undefined:
+        case undefined || null:
           jobDescription = null;
           break;
 
@@ -73,7 +62,7 @@ const resumeController = {
 
       var industry;
       switch (optionalIndustry) {
-        case undefined:
+        case undefined || null:
           industry = null;
           break;
 
@@ -117,9 +106,8 @@ const resumeController = {
       }
 
       // create prompts to send to openAI API as user
-      const role1Prompt = `Write me a 50 word description about my role at ${company1} in ${location1} where I worked as a ${role1}. I was responsible for ${description1}`;
-      const role2Prompt = `Write me a 50 word description about my role at ${company2} in ${location2} where I worked as a ${role2}. I was responsible for ${description2}`;
-      const educationPrompt = `Write 5 courses that I might have taken at ${schoolName} in ${location}, getting my degree in ${degree}. My graduation date is ${graduation}`;
+      const experiencePrompt = `Write me a 50 word description about my role at ${company} in ${location} where I worked as a ${role}. I was responsible for ${description}. I worked here during ${date}`;
+      const educationPrompt = `Write 5 courses that I might have taken at ${schoolName} in ${schoolLocation}, getting my degree in ${degree}. My graduation date is ${graduation}`;
       const skillsPrompt = `Write 10 points for my resume on what I am good at given my skills, ${skills}`;
       const projectPrompt = `Write me a 50 word description about my project called ${projectTitle} that consisted of ${projectDescription}`;
 
@@ -127,14 +115,9 @@ const resumeController = {
       const systemMessage = { role: "system", content: systemPrompt };
 
       // generate all resume info
-      const role1Generated = await generateText([
+      const experienceGenerated = await generateText([
         systemMessage,
-        { role: "user", content: role1Prompt },
-      ]);
-
-      const role2Generated = await generateText([
-        systemMessage,
-        { role: "user", content: role2Prompt },
+        { role: "user", content: experiencePrompt },
       ]);
 
       const educationGenerated = await generateText([
@@ -153,8 +136,7 @@ const resumeController = {
       ]);
 
       const chatgptData = {
-        role1Generated,
-        role2Generated,
+        experienceGenerated,
         educationGenerated,
         skillsGenerated,
         projectGenerated,
