a6f021db35b8d50d3d81218f0dcb34ae4cebc065
dalkwifi <dalkwifi@uwo.ca>
2023-12-05 15:00:21 -0500

    ExperincFixed

--- resubot-app/src/pages/FinalResume.jsx
+++ resubot-app/src/pages/FinalResume.jsx
@@ -374,7 +374,7 @@ const FinalResume = () => {
               type="text"
               name="date"
               placeholder="May 23 - Present"
-              className="currentInput"
+              className="subInput"
               value={start}
               required
               onChange={(e) => setStart(e.target.value)}
@@ -385,7 +385,7 @@ const FinalResume = () => {
             <input
               type="text"
               name="location"
-              className="currentInput"
+              className="subInput"
               value={location}
               required
               onChange={(e) => setLocation(e.target.value)}
