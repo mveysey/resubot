bce434adb73ba72b49cb9a328eca824af6e08cfe
dalkwifi <dalkwifi@uwo.ca>
2024-03-05 20:01:08 -0500

    navbar in progress

--- resubot-app/src/components/Header/Header.jsx
+++ resubot-app/src/components/Header/Header.jsx
@@ -1,26 +1,65 @@
-import React, { useState } from 'react';
-import './Header.scss';
+import React, { useMemo, useState } from 'react';
+import logo from '../../assets/logo.png';
+import "./Header.scss";
+import auth from "../../common/auth.js";
+import SlidingPane from "react-sliding-pane";
+import UserProfile from "../UserProfile/UserProfile.jsx";
 
-const Header = () => {
+const Header = ({ version }) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <nav className="navbar">
-      <div className="logo">Logo</div>
-      <div className={`nav-items ${isOpen ? 'open' : ''}`}>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/services">Services</a>
-        <a href="/contact">Contact</a>
-        <div className="close-icon" onClick={() => setIsOpen(false)}>X</div> {/* Add this line */}
+  let user = useMemo(() => auth.getUser() || {}, []);
 
-      </div>
-      <div className="burger" onClick={() => setIsOpen(!isOpen)}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </nav>
+  const [state, setState] = useState({
+    isPaneOpen: false,
+    isPaneOpenLeft: false,
+  });
+
+  return (
+    <>
+      <SlidingPane
+        closeIcon={<div>x</div>}
+        className="section panel"
+        overlayClassName="overlay"
+        isOpen={state.isPaneOpen}
+        width="500px"
+        onRequestClose={() => setState({ isPaneOpen: false })}
+      >
+        <UserProfile user={user} close={() => setState({ isPaneOpen: false })} />
+      </SlidingPane>
+      <nav className="navbar">
+        <a className="navbar-brand" href="/">
+          <img src={logo} alt="Logo" width="190" height="100" />
+        </a>
+        <div className={`nav-items ${isOpen ? 'open' : ''}`}>
+          <a className="navbar-item" href="/chatbot">Chatbot</a>
+          <a className="navbar-item" href="/home">Profile</a>
+          <a className="navbar-item" href="/contact">Help</a>
+          <div className="navbar-end">
+            {user?.username ? (
+              <div className="navbar-item" onClick={() => setState({ isPaneOpen: true })}>
+                <span className="user-icon">{user?.username.slice(0, 1)}</span>
+                <span>{user?.username.slice(0, 10)}</span>
+              </div>
+            ) : (
+              <>
+                <div className="navbar-item">
+                  <a href="/login">Login</a>
+                </div>
+                <div className="navbar-item">
+                  <a href="/register">Register</a>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="burger" onClick={() => setIsOpen(!isOpen)}>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </nav>
+    </>
   );
 };
 
