6a774ef567939139550d5a789e0ff2142f6a7158
Mark Yu <zyu347@uwo.ca>
2024-03-21 03:12:40 -0400

    Fixed auth for sql server 2022

---
+++ Server/routes/admin.router.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+const adminController = require("../controller/admin.controller")
+
+
+router.get("/skills", )
+
+
+module.exports = router
---
+++ resubot-app/src/pages/Admin/Admin.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Admin.scss'
+
+
+//Admin Page
+const AdminPage = () =>
+    (
+        <div className="admin-wrapper">
+            <div className="container">
+                <div className="notification is-dark">
+                   Welcome to <strong> Admin Dashboard</strong>
+                </div>
+
+
+            </div>
+        </div>
+    )
+
+
+export default AdminPage
